b24fc850a69b7ccebd8bf42e01d3b340ecb0db8a
finalgtx <finalgtx.0710@gmail.com>
2026-05-11 08:51:36 +0900

    fix images.js with new assets

--- src/data/images.js
+++ src/data/images.js
@@ -1,24 +1,17 @@
-// src/data/images.js
-// 各イベント・敵・エリアの画像URL
-// ※ URLは自由に変更してください（フリー素材など）
-
 export const IMAGES = {
-  // エリア画像
   areas: {
-    starting_village:   'https://cdn.pixabay.com/photo/2021/11/07/09/28/village-6776719_640.jpg',
+    starting_village:   'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/backgrounds/starting_village.png',
     forest_of_whispers: 'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/backgrounds/forest_of_whispers.png',
     ancient_ruins:      'https://cdn.pixabay.com/photo/2016/11/14/04/45/ancient-1822471_640.jpg',
     crystal_cavern:     'https://cdn.pixabay.com/photo/2016/03/27/22/02/cave-1283746_640.jpg',
     ether_sanctuary:    'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_640.jpg',
   },
-
-  // 敵画像
   enemies: {
-　　slime: 　　　　　'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/monsters/slime.png',
-    goblin:         'https://cdn.pixabay.com/photo/2014/04/03/10/28/goblin-310662_640.png',
+    slime:          'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/monsters/slime.png',
+    goblin:         'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/monsters/goblin.png',
     wolf:           'https://cdn.pixabay.com/photo/2016/03/31/19/48/animal-1295115_640.png',
     forest_sprite:  'https://cdn.pixabay.com/photo/2017/01/31/13/14/fairy-2024022_640.png',
-    dark_goblin:    'https://cdn.pixabay.com/photo/2014/04/03/10/28/goblin-310662_640.png',
+    dark_goblin:    'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/monsters/goblin.png',
     stone_golem:    'https://cdn.pixabay.com/photo/2016/04/01/09/30/golem-1299051_640.png',
     ruin_guardian:  'https://cdn.pixabay.com/photo/2016/04/01/09/30/golem-1299051_640.png',
     crystal_golem:  'https://cdn.pixabay.com/photo/2016/04/01/09/30/golem-1299051_640.png',
@@ -28,20 +21,11 @@ export const IMAGES = {
     void_knight:    'https://cdn.pixabay.com/photo/2014/04/03/10/32/dragon-312476_640.png',
     ancient_dragon: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/dragon-312476_640.png',
   },
-
-  // ボス画像
   bosses: {
-    goblin_king:     'https://cdn.pixabay.com/photo/2014/04/03/10/28/goblin-310662_640.png',
-    forest_guardian: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/forest-1072828_640.jpg',
+    goblin_king:      'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/monsters/goblin.png',
+    forest_guardian:  'https://raw.githubusercontent.com/finalgtx0710-hash/discord-rpg-bot/main/assets/backgrounds/forest_of_whispers.png',
     ancient_overlord: 'https://cdn.pixabay.com/photo/2014/04/03/10/32/dragon-312476_640.png',
   },
-
-  // イベント画像
   events: {
     treasure: 'https://cdn.pixabay.com/photo/2013/07/13/13/44/chest-161724_640.png',
-    heal:     'https://cdn.pixabay.com/photo/2016/03/31/14/37/water-1292908_640.png',
-    npc:      'https://cdn.pixabay.com/photo/2016/04/01/09/60/cartoon-1299043_640.png',
-    victory:  'https://cdn.pixabay.com/photo/2016/03/31/19/48/achievement-1295079_640.png',
-    defeat:   'https://cdn.pixabay.com/photo/2016/03/31/14/37/skull-1292917_640.png',
-  },
-};
+    heal:     'htt
